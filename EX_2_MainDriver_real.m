b4bc4a128dcce967c97f64e1c5ef09fb2798f7a2
T-Cho-vt <52902993+T-Cho-vt@users.noreply.github.com>
2021-04-02 17:56:13 -0400

    Add files via upload

--- EX_2_MainDriver_real.m
+++ EX_2_MainDriver_real.m
@@ -60,28 +60,25 @@ Abdo = resample(Abdo(:,2),1,3);
 Abdo = Abdo(1:sig_size);
 
 % Load photo of living organism in respirometry chamber
-Insect_color = imread('Insect_color.jpg');
-Insect_color = Insect_color(361:800,301:end-360,:);
-Insect_red = imread('Insect_red.jpg');
-Insect_red = Insect_red(24:end-51,81:end-80,:);
-Insect_red = Insect_red(210:465,175:end-209,:);
+Beetle_abdo = imread('Beetle_abdo.jpg');
+Beetle_points = imread('Beetle_points.jpg');
 
 recon_sig = blind_x_gamma(1:sig_size);
 observ_sig = params.b(1:sig_size);
 
 figure, 
     
-    subplot(6,2,1), hold on
-        imshow(Insect_color),
+    subplot(7,2,[1; 3]), hold on
+        imshow(Beetle_abdo),
         text(0,-70,'(a)','fontsize',20),
     hold off
     
-    subplot(6,2,2), hold on
-        imshow(Insect_red),
-        text(0,-30,'(b)','fontsize',20),
+    subplot(7,2,[2; 4]), hold on
+        imshow(Beetle_points),
+        text(0,-70,'(b)','fontsize',20),
     hold off
     
-    subplot(6,2,[3 4]), hold on,
+    subplot(7,2,[3 4] +2), hold on,
         plot(Abdo,'color',[0.8500, 0.3250, 0.0980], 'linewidth', 1.5),
         text(0,.95,'(c)','fontsize',20),
     hold off
@@ -90,7 +87,7 @@ figure,
     set(gca,'ytick',[])
     axis([1 sig_size 0.1 1.2*max(Abdo(:))]);
     
-    subplot(6,2,[5 6]), hold on,
+    subplot(7,2,[5 6] +2), hold on,
         plot(observ_sig,'color',[0,0,0]+0.45,'linewidth', 1.5),
         text(0,0.8,'(d)','fontsize',20),
     hold off
@@ -99,7 +96,7 @@ figure,
     set(gca,'ytick',[])
     axis([1 sig_size -0.01 1.2*max(observ_sig(:))]);
     
-    subplot(6,2,[7 8]), hold on,
+    subplot(7,2,[7 8] +2), hold on,
         plot(recon_sig,'color',[0, 0.4470, 0.7410],'linewidth', 1.2),
         text(0,1.75,'(e)','fontsize',20),
     hold off
@@ -108,7 +105,7 @@ figure,
     set(gca,'ytick',[])
     axis([1 sig_size -0.01 1.2*max(recon_sig(:))]);
     
-    subplot(6,2,[9 10 11 12]), hold on,
+    subplot(7,2,[9 10 11 12] +2), hold on,
         plot(observ_sig,'color',[0,0,0]+0.45,'linewidth', 1.5),
         plot(Abdo+0.85,'color',[0.8500, 0.3250, 0.0980],'linewidth', 1.5),
         plot(recon_sig,'color',[0, 0.4470, 0.7410],'linewidth', 1.2),
@@ -122,13 +119,13 @@ set(gcf,'Position',[50 100 800 1000])
 
 %% Plot reconstructions of h(t)
 % Figure 14 in paper
-h_unif = ones(params.supp,1)/params.supp; % initial guess
-n2 = floor(params.supp/2);
-h0 = linspace(2/params.supp,0,n2)'; h0 = [h0; zeros(params.supp-n2,1)];% initial guess
+h_unif = ones(Params.supp,1)/Params.supp; % initial guess
+n2 = floor(Params.supp/2);
+h0 = linspace(2/Params.supp,0,n2)'; h0 = [h0; zeros(Params.supp-n2,1)];% initial guess
 h_desc = fliplr(h0);
-h_exp = params.ht(params.delay+1:params.delay+params.supp)/sum(params.ht); % initial guess
-[~,h_gam1,~]=h_Gamma([3 3], 25, 10, 0.0001); h_gam1= h_gam1/sum(h_gam1); h_gam1 = h_gam1(1:params.supp);
-[~,h_gam2,~]=h_Gamma([1 .5], 25, 10, 0.0001); h_gam2= h_gam2/sum(h_gam2); h_gam2 = h_gam2(1:params.supp); figure, plot(h_gam2);
+h_exp = Params.ht(Params.delay+1:Params.delay+Params.supp)/sum(Params.ht); % initial guess
+[~,h_gam1,~]=h_Gamma([3 3], 25, 10, 0.0001); h_gam1= h_gam1/sum(h_gam1); h_gam1 = h_gam1(1:Params.supp);
+[~,h_gam2,~]=h_Gamma([1 .5], 25, 10, 0.0001); h_gam2= h_gam2/sum(h_gam2); h_gam2 = h_gam2(1:Params.supp); figure, plot(h_gam2);
 subplot(1,2,1)
 hold on
     plot(h_unif,'linewidth',2.5,'color',[0.9290, 0.6940, 0.1250])
@@ -137,8 +134,10 @@ hold on
 hold off
 legend('Flat line','Gamma1','Gamma2','fontsize',20)
 title('Initital h','fontsize',25)
-xlim([1 params.supp])
+xlim([1 Params.supp])
 ylim([0 0.07])
+set(gca,'xtick',[1 Params.supp/2 Params.supp],'xticklabel',{num2str((Params.delay)/10),num2str((Params.delay+Params.supp/2)/10),[num2str((Params.delay+Params.supp)/10),'(s)']},'fontsize',15)
+set(gca,'ytick', 0.01:0.01:0.07)
 set(gca,'fontsize',20)
 
 subplot(1,2,2)
@@ -147,8 +146,10 @@ hold on
     plot(blind_h_gamma,'--','linewidth',2.5,'color',[0.8500, 0.3250, 0.0980])
     plot(blind_h_gamma2,':','linewidth',2.5,'color',[0.4940, 0.1840, 0.5560])
 hold off
-xlim([1 params.supp])
+xlim([1 Params.supp])
 ylim([0 0.07])
+set(gca,'xtick',[1 Params.supp/2 Params.supp],'xticklabel',{num2str((Params.delay)/10),num2str((Params.delay+Params.supp/2)/10),[num2str((Params.delay+Params.supp)/10),'(s)']},'fontsize',15)
+set(gca,'ytick', 0.01:0.01:0.07)
 legend('Flat line - est','Gamma1 - est','Gamma2 - est','fontsize',20)
 title('Reconstructed h','fontsize',25)
 set(gca,'fontsize',20)
